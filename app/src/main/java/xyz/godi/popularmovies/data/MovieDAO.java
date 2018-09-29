e8668a34dd121c74809b990bd00b4d75e585a77c
Farouk <sfarouk298@gmail.com>
2018-09-29 03:54:47 +0100

    some changes

--- app/src/main/java/xyz/godi/popularmovies/data/MovieDAO.java
+++ app/src/main/java/xyz/godi/popularmovies/data/MovieDAO.java
@@ -6,17 +6,23 @@ import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.OnConflictStrategy;
 
+import java.util.List;
+
 import xyz.godi.popularmovies.model.FavoriteMovie;
 import xyz.godi.popularmovies.model.Movie;
 
 @Dao
 public interface MovieDAO {
 
+    // get all movies
+    @Query("SELECT * FROM movie")
+    List<Movie> getAll();
+
     // insert movie into the database
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Movie movie);
 
-    @Query("SELECT id FROM Movie Where id = :id")
+    @Query("SELECT id FROM movie Where id = :id")
     FavoriteMovie getMovieById(String id);
 
     @Delete

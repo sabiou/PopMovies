8891f0941895805231b3a8204d11e9bde1a627b2
Farouk <sfarouk298@gmail.com>
2018-09-30 21:48:05 +0100

    Performed some changes in MovieViewModel.java

---
+++ app/src/main/java/xyz/godi/popularmovies/viewModel/MovieViewModel.java
@@ -0,0 +1,31 @@
+package xyz.godi.popularmovies.viewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import xyz.godi.popularmovies.data.MovieRepository;
+import xyz.godi.popularmovies.model.Movie;
+
+public class MovieViewModel extends AndroidViewModel {
+
+    private MovieRepository movieRepository;
+    private LiveData<List<Movie>> allMovies;
+
+    public MovieViewModel(@NonNull Application application) {
+        super(application);
+        movieRepository = new MovieRepository(application);
+        allMovies = movieRepository.getMovies();
+    }
+
+    public LiveData<List<Movie>> getAllMovies() {
+        return allMovies;
+    }
+
+    public void insert(Movie movie) {
+        movieRepository.insert(movie);
+    }
+}

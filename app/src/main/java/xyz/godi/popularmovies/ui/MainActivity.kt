ac8d8dd5c9b902ae10e4ff117d5abbf6985c34af
Farouk <sfarouk298@gmail.com>
2020-06-19 04:22:48 +0100

    Added - Viewbinding to MainActivity

--- app/src/main/java/xyz/godi/popularmovies/ui/MainActivity.kt
+++ app/src/main/java/xyz/godi/popularmovies/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package xyz.godi.popularmovies.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import xyz.godi.popularmovies.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding

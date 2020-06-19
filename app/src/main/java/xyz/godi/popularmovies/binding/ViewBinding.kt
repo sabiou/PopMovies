f40b9026fd335b91b10cd0e0b38d932b60323ea5
Farouk <sfarouk298@gmail.com>
2020-06-19 04:18:40 +0100

    Added - poster image binding adapter

--- app/src/main/java/xyz/godi/popularmovies/binding/ViewBinding.kt
+++ app/src/main/java/xyz/godi/popularmovies/binding/ViewBinding.kt
@@ -3,10 +3,13 @@ package xyz.godi.popularmovies.binding
 import android.app.Activity
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.isGone
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
+import com.bumptech.glide.Glide
 import com.skydoves.whatif.whatIfNotNull
+import xyz.godi.popularmovies.utils.PostersUtils
 
 /**
  * Created by Farouk on 12/06/20.
@@ -32,4 +35,12 @@ fun bindOnBackPressed(view: View, finish: Boolean) {
             context.onBackPressed()
         }
     }
+}
+
+@BindingAdapter("posterImage")
+fun bindPosterImage(view: AppCompatImageView, url: String?) {
+    val context = view.context
+    Glide.with(context)
+            .load(url?.let { PostersUtils.getPosterPath(it) })
+            .into(view)
 }

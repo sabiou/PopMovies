220714e5314466352528286db0cab2cb26724276
Farouk <sfarouk298@gmail.com>
2020-06-19 04:16:29 +0100

    Added - apikey to the request interceptor

--- app/src/main/java/xyz/godi/popularmovies/network/HttpRequestInterceptor.kt
+++ app/src/main/java/xyz/godi/popularmovies/network/HttpRequestInterceptor.kt
@@ -2,6 +2,7 @@ package xyz.godi.popularmovies.network
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import xyz.godi.popularmovies.BuildConfig
 
 /**
  * Created by Farouk on 12/06/20.
@@ -9,7 +10,12 @@ import okhttp3.Response
 class HttpRequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        val request = originalRequest.newBuilder().url(originalRequest.url).build()
+        val originalUrl = originalRequest.url
+        val url = originalUrl.newBuilder()
+                .addQueryParameter("api_key", BuildConfig.API_KEY)
+                .build()
+        val requestBuilder = originalRequest.newBuilder().url(url)
+        val request = requestBuilder.build()
         // Timber.d(request.toString())
         return chain.proceed(request)
     }
